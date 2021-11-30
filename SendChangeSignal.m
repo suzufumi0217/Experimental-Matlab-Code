5cb4aee1c9b3fdd9120709709d1ed5cce45bebad
Arai suzufumi <suzufumi@bpe.es.osaka-u.ac.jp>
2021-11-30 10:35:03 +0900

    Change : Stimulate Sol to initiate step

--- SendChangeSignal.m
+++ SendChangeSignal.m
@@ -10,12 +10,12 @@ function info = SendChangeSignal(motionstim8, info, mainleg, state)
 % define which channel corresponds with which muscle
 channelList.right.BF = 1;
 channelList.right.VM = 2;
-channelList.right.SO = 3;
-channelList.right.TA = 4;
+channelList.right.TA = 3;
+channelList.right.SO = 4;
 channelList.left.BF = 5;
 channelList.left.VM = 6;
-channelList.left.SO = 7;
-channelList.left.TA = 8;
+channelList.left.TA = 7;
+channelList.left.SO = 8;
 
 
 % identify which leg is being used
@@ -29,8 +29,8 @@ end
 
 % identify which channels are to be turned on/off
 if state == 3 % Swing back
-    on = sort([channelList.(sublegStr).BF channelList.(sublegStr).VM  channelList.(sublegStr).TA]);
-    off = sort([channelList.(mainlegStr).BF channelList.(mainlegStr).VM channelList.(mainlegStr).SO channelList.(mainlegStr).TA channelList.(sublegStr).SO]);
+    on = sort([channelList.(mainlegStr).SO channelList.(sublegStr).BF channelList.(sublegStr).VM  channelList.(sublegStr).TA]);
+    off = sort([channelList.(mainlegStr).BF channelList.(mainlegStr).VM channelList.(mainlegStr).TA channelList.(sublegStr).SO]);
     
 elseif state == 4 % Push off
     on = sort([channelList.(mainlegStr).TA channelList.(sublegStr).SO]);
