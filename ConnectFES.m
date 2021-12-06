306fed293622e90e1baf6d7096f15a2893a44c32
Arai suzufumi <suzufumi@bpe.es.osaka-u.ac.jp>
2021-12-06 16:04:48 +0900

    Change : FES configure code for using 8 channels

--- ConnectFES.m
+++ ConnectFES.m
@@ -13,8 +13,8 @@ info.AllChannels = [1 2 3 4 5 6 7 8]; % List of all channels that will be used f
 info.lowFreqChannels = []; % Channels to use low stimulation on - currently not being used
 info.PulseWidth = 200; % in microseconds
 info.Mode = 0; % 0 = single pulse, 1 = doublet, 2 = triplet
-info.ts1 = 10; % Main time in ms
-info.ts2 = 6.0; % Group time in ms(number of channel*1.5)
+info.ts1 = 25; % Main time in ms %stimulation in 40Hz in this situation   
+info.ts2 = 12; % Group time in ms(number of channel*1.5)
 info.N = 1; % channels will be stimulated every Nth time, should be 1 for current purposes
 info.currentAmplitudes = zeros(1,length(info.AllChannels)); % start at 0 - no channels are on!
 info.rampUpTime = 0.3; % time in seconds for up ramps
